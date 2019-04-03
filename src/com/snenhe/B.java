02fc72a15c2f4a1724ed40e6292dbd3dff4dfe53
jingyongqi <1370305057@QQ.com>
2019-04-03 10:59:11 +0800

    ing勇琪

---
+++ src/com/snenhe/B.java
@@ -0,0 +1,14 @@
+package com.snenhe;
+
+/**
+ * 类名:SystemAccountDao<br>
+ * 功能:对于后台系统用户的操作<br>
+ * 作者:java战士<br>
+ * 日期:2019/3/26<br>
+ * 版本:1.0.0<br>
+ */
+public class B {
+    public static  void main(String [] args){
+        System.out.println("fjsdklfsajlksafsdf");
+    }
+}
